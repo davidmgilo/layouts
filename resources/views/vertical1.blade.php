e03a71f0fd0851e224c87495eea3bce5c6c0ba23
David Martinez <davidmgilo@gmail.com>
2017-01-24 16:41:27 +0100

    sync

--- resources/views/vertical1.blade.php
+++ resources/views/vertical1.blade.php
@@ -6,16 +6,16 @@
 
 
 @section('main-content')
-	<div class="container-fluid spark-screen">
-		<div class="row">
-			<div class="col-md-8 col-md-offset-2">
-				<div class="panel panel-default">
-					<div class="panel-heading">Change Header</div>
-					<div class="panel-body">
-						Change content here!
-					</div>
-				</div>
-			</div>
-		</div>
+
+<style>
+
+.totalcentered {
+	border: 1px black solid;
+}
+
+</style>
+	<div class="totalcentered">
+		Hello world!!
 	</div>
+
 @endsection
