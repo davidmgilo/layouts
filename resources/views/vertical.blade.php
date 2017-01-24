16d37d3e1cd6d6257ea7130e251c995d47cb060f
David Martinez <davidmgilo@gmail.com>
2017-01-24 16:18:25 +0100

    contiguous blocks

--- resources/views/vertical.blade.php
+++ resources/views/vertical.blade.php
@@ -20,12 +20,14 @@ body {
 	text-align: center;
 	max-width: 500px;
 	margin: 0 auto;
+	display: inline-block;
 }
 
 
 </style>
 
 <body>
+<div id="bloc">Prova</div>
 <div id="bloc">Prova</div>
 </body>
 </html>
